a5f5201e9cacb1587b0101e2e182307b638e172f
Tom Feist <shabble@metavore.org>
2010-10-06 18:21:24 +0100

    documentation improvements

--- feature-tests/sbar_test.pl
+++ feature-tests/sbar_test.pl
@@ -29,3 +29,47 @@ sub foo_sb {
 }
 
 Irssi::statusbar_item_register ('foo_bar', 0, 'foo_sb');
+
+__END__
+# Name                           Type   Placement Position Visible
+# window                         window bottom    1        active
+# window_inact                   window bottom    1        inactive
+# prompt                         root   bottom    0        always
+# topic                          root   top       1        always
+
+# Statusbar: prompt
+# Type     : root
+# Placement: bottom
+# Position : 0
+# Visible  : always
+# Items    : Name                                Priority  Alignment
+#          : prompt                              0         left
+#          : prompt_empty                        0         left
+#          : input                               10        left
+#
+# STATUSBAR <name> ENABLE
+# STATUSBAR <name> DISABLE
+# STATUSBAR <name> RESET
+# STATUSBAR <name> TYPE window|root
+# STATUSBAR <name> PLACEMENT top|bottom
+# STATUSBAR <name> POSITION <num>
+# STATUSBAR <name> VISIBLE always|active|inactive
+# STATUSBAR <name> ADD
+#                  [-before | -after <item>] [-priority #]
+#                  [-alignment left|right] <item>
+#
+# STATUSBAR <name> REMOVE <item>
+#
+# Commands for modifying the statusbar.
+#
+# /STATUSBAR
+#    - Display all statusbars.
+#
+# /STATUSBAR <name>
+#    - display elements of statusbar <name>
+#
+# Irssi commands:
+# statusbar add     statusbar enable    statusbar position  statusbar reset
+# statusbar visible statusbar disable   statusbar placement statusbar remove
+# statusbar type
+
