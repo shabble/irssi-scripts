shabble/irssi-scripts

commit d736f605126e368124b3ff8ed4db5b28f83b650f

vim_mode: Add : to $commands_ex's {char}.

vim-mode/vim_mode.pl
@@ -359,26 +359,26 @@ my $commands
 # All available commands in Ex-Mode.
 my $commands_ex
   = {
-     s         => { char => 's',         func => \&ex_substitute, type => C_EX },
-     bnext     => { char => 'bnext',     func => \&ex_bnext,      type => C_EX },
-     bn        => { char => 'bn',        func => \&ex_bnext,      type => C_EX },
-     bprev     => { char => 'bprev',     func => \&ex_bprev,      type => C_EX },
-     bp        => { char => 'bp',        func => \&ex_bprev,      type => C_EX },
-     bdelete   => { char => 'bdelete',   func => \&ex_bdelete,    type => C_EX },
-     bd        => { char => 'bd',        func => \&ex_bdelete,    type => C_EX },
-     buffer    => { char => 'buffer',    func => \&ex_buffer,     type => C_EX },
-     b         => { char => 'b',         func => \&ex_buffer,     type => C_EX },
-     registers => { char => 'registers', func => \&ex_registers,  type => C_EX },
-     reg       => { char => 'reg',       func => \&ex_registers,  type => C_EX },
-     display   => { char => 'display',   func => \&ex_registers,  type => C_EX },
-     di        => { char => 'di',        func => \&ex_registers,  type => C_EX },
-     buffers   => { char => 'buffer',    func => \&ex_buffers,    type => C_EX },
-     ls        => { char => 'ls',        func => \&ex_buffers,    type => C_EX },
-     undolist  => { char => 'undolist',  func => \&ex_undolist,   type => C_EX },
-     undol     => { char => 'undol',     func => \&ex_undolist,   type => C_EX },
-     map       => { char => 'map',       func => \&ex_map,        type => C_EX },
-     source    => { char => 'source',    func => \&ex_source,     type => C_EX },
-     so        => { char => 'so',        func => \&ex_source,     type => C_EX },
+     s         => { char => ':s',         func => \&ex_substitute, type => C_EX },
+     bnext     => { char => ':bnext',     func => \&ex_bnext,      type => C_EX },
+     bn        => { char => ':bn',        func => \&ex_bnext,      type => C_EX },
+     bprev     => { char => ':bprev',     func => \&ex_bprev,      type => C_EX },
+     bp        => { char => ':bp',        func => \&ex_bprev,      type => C_EX },
+     bdelete   => { char => ':bdelete',   func => \&ex_bdelete,    type => C_EX },
+     bd        => { char => ':bd',        func => \&ex_bdelete,    type => C_EX },
+     buffer    => { char => ':buffer',    func => \&ex_buffer,     type => C_EX },
+     b         => { char => ':b',         func => \&ex_buffer,     type => C_EX },
+     registers => { char => ':registers', func => \&ex_registers,  type => C_EX },
+     reg       => { char => ':reg',       func => \&ex_registers,  type => C_EX },
+     display   => { char => ':display',   func => \&ex_registers,  type => C_EX },
+     di        => { char => ':di',        func => \&ex_registers,  type => C_EX },
+     buffers   => { char => ':buffer',    func => \&ex_buffers,    type => C_EX },
+     ls        => { char => ':ls',        func => \&ex_buffers,    type => C_EX },
+     undolist  => { char => ':undolist',  func => \&ex_undolist,   type => C_EX },
+     undol     => { char => ':undol',     func => \&ex_undolist,   type => C_EX },
+     map       => { char => ':map',       func => \&ex_map,        type => C_EX },
+     source    => { char => ':source',    func => \&ex_source,     type => C_EX },
+     so        => { char => ':so',        func => \&ex_source,     type => C_EX },
     };
 
 # MAPPINGS
@@ -1613,12 +1613,8 @@ sub ex_map {
             my $cmd = $map->{cmd};
             if (defined $cmd) {
                 next if $map->{char} eq $cmd->{char}; # skip default mappings
-
-                my $cmdc = $cmd->{char};
-                if ($cmd->{type} == C_EX) {
-                    $cmdc = ":$cmdc";
-                }
-                $active_window->print(sprintf "%-15s %s", $map->{char}, $cmdc);
+                $active_window->print(sprintf "%-15s %s", $map->{char},
+                                                          $cmd->{char});
             }
         }
     } else {

commit ff5fb6c40953421e40f4118473547096474f35de

vim_mode: Fix :map with ex-commands.
Broken in d736f605126e368124b3ff8ed4db5b28f83b650f.

vim-mode/vim_mode.pl
@@ -2072,7 +2072,7 @@ sub handle_command_cmd {
     # ex-mode command doesn't need any additional arguments.
     if ($cmd->{type} == C_EX) {
         print "Processing ex-command: $map->{char} ($cmd->{char})" if DEBUG;
-        $cmd->{func}->($cmd->{char});
+        $cmd->{func}->(substr $cmd->{char}, 1);
         return 1; # call _stop()
     # As can irssi commands.
     } elsif ($cmd->{type} == C_IRSSI) {