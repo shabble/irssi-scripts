9b79dbd185e78a7cd288c88a31a188b4be62c698
Tom Feist <shabble@metavore.org>
2010-11-05 05:06:35 +0000

    url_hilight: added a warning about unfinishedness, and make it at least compile.

--- url_hilight/url_hilight.pl
+++ url_hilight/url_hilight.pl
@@ -1,3 +1,11 @@
+# url_hilight.pl
+#
+# This script is a work in progress, and will probably not do anything useful
+# at the moment.  Please fork and implement, or bug me on #irssi if you can
+# decide what it ought to do, since I've forgotten.
+
+
+
 use strict;
 use warnings;
 use Irssi;
@@ -6,9 +14,9 @@ our %IRSSI = (
              "author"      => 'shabble',
              "contact"     => 'shabble+irssi@metavore.org, shabble@Freenode/#irssi',
              "name"        => 'URL Highlighter',
-             "description" => ' '
-             "license"     => 'WTFPL: http://sam.zoy.org/wtfpl/'
-             "version"     => '0.1'
+             "description" => ' ',
+             "license"     => 'WTFPL: http://sam.zoy.org/wtfpl/',
+             "version"     => '0.1',
             );
 
 # regex taken from http://daringfireball.net/2010/07/improved_regex_for_matching_urls
@@ -20,7 +28,6 @@ sub handle_gui_print_text {
         print "matched!";
     }
 }
-
 
 Irssi::signal_add('gui print text', \&handle_gui_print_text);
 
