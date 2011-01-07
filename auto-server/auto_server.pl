be164408bab4128c4e67b6ac2e0f0183702c1790
Rich Healey <richh@boxdice.com.au>
2011-01-07 14:13:41 +1100

    - is a valid character in a channel name.

--- auto-server/auto_server.pl
+++ auto-server/auto_server.pl
@@ -135,7 +135,7 @@ sub join_plus {
 
     # parse out channel name from args:
     my $channel;
-    if ($args =~ m/^(#?[#a-zA-Z0-9]+)/) {
+    if ($args =~ m/^(#?[#a-zA-Z0-9-]+)/) {
         $channel = $1;
         _debug_print ("Channel is: $channel");
     }
