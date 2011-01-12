d285e7854dfc62c213bec4c101eaf080fa81ac46
Tom Feist <shabble@metavore.org>
2011-01-12 00:10:16 +0000

    pipes: added an example feature test on how to use pipes for non-blocking IO. Hope to expand it to include network IO at some point.

--- feature-tests/format-test.pl
+++ feature-tests/format-test.pl
@@ -3,7 +3,7 @@ use warnings;
 
 
 use Irssi;
-
+use Data::Dumper;
 
 our $VERSION = '0.1';
 our %IRSSI = (
@@ -16,19 +16,24 @@ our %IRSSI = (
 
 sub actually_printformat {
     my ($win, $level, $module, $format, @args) = @_;
+    my $ret = '';
     {
         # deeeeeeep black magic.
         local *CORE::GLOBAL::caller = sub { $module };
         $win->printformat($level, $format, @args);
-    }
 
+        $ret = Irssi::current_theme()->get_format($module, $format);
+    }
+    return $ret;
 }
 
 init();
 
 sub init {
     my $win = Irssi::active_win();
-    actually_printformat($win, Irssi::MSGLEVEL_CLIENTCRAP, 'fe-common/irc',
-                               "kill_server", "foo", "bar", "horse", "cake");
+    my $moo = actually_printformat($win, Irssi::MSGLEVEL_CLIENTCRAP, 'fe-common/irc',
+                                   "kill_server", "foo", "bar", "horse", "cake");
+
+    print Dumper($moo);
 
 }
