f2d7b83e5d84c75510372ca9ba575181d35d20aa
Tom Feist <shabble@metavore.org>
2011-04-09 17:26:38 +0100

    redir-input: test of some internal callback stuff. Which doesn't work. Ho Hum.

--- feature-tests/redir-input.pl
+++ feature-tests/redir-input.pl
@@ -20,11 +20,14 @@ our %IRSSI = (
 
 
 Irssi::command_bind("ri", \&cmd_ri);
-
+Irssi::signal_register({ 'gui entry redirect' => [ qw/string string intptr intptr/]});
 sub cmd_ri {
     my ($args, $server, $witem) = @_;
     my $win = Irssi::active_win();
-
+    my ($x, $y) = (0, 0);
+    Irssi::signal_emit('gui entry redirect', 'sub_blah', "bacon", $x, $y);
     #my $ref = Irssi::windows_refnum_last
-    $win->format_create_dest(Irssi::MSGLEVEL_ClIENTCRAP());
+#    $win->format_create_dest(Irssi::MSGLEVEL_ClIENTCRAP());
 }
+
+sub sub_blah { print "Moo" }
